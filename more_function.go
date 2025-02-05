a4c2fbba81edd0b764e01e8ed77e427c1f81d785
mohiuddin1221 <topumohiuddin3@gmail.com>
2025-02-05 15:05:34 +0600

    Multiple type function

--- more_function.go
+++ more_function.go
@@ -1,50 +1,26 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
-func WelcomeMessage() {
-	fmt.Println("Welcome to the Application")
+// Standard function
+func add(x int, y int){
+	fmt.Println(x + y)
 }
 
-func GetUserName() string {
-	var name string
-	fmt.Println("Please Enter your Name:")
-	fmt.Scanln(&name)
-	return name
+func main(){
+	add(5, 10)
+	// Anonymous function
+	// IIFS Function immediately invokes a function expression
+	func (x int, y int){
+		fmt.Println(x + y)
+	}(30, 40)
+
+    
 }
 
-func GetTwoNumbers() (int, int) {
-	var num1 int
-	var num2 int
-	fmt.Println("Enter Your First Number:")
-	fmt.Scanln(&num1)
-	fmt.Println("Enter Your Second Number:")
-	fmt.Scanln(&num2)
-	return num1, num2
-}
-
-func SumNumbers(num1 int, num2 int) int {
-	sum := num1 + num2
-	return sum
-}
-
-func Display(name string, sum int) {
-	fmt.Println("Hello", name)
-	fmt.Println("The sum of numbers is:", sum)
-}
-
-func GoodBye() {
-	fmt.Println("Thank you for using the Application")
-	fmt.Println("GoodBye!")
-}
-
-func main() {
-	WelcomeMessage()
-	name := GetUserName()
-	num1, num2 := GetTwoNumbers()
-	sum := SumNumbers(num1, num2)
-	Display(name, sum)
-	GoodBye()
+func init(){
+	fmt.Println("First Print invoke function")
+	
 }
